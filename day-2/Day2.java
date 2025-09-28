f933ef3ec3bbcc69a183d4d5139fda25b0a4e968
Isuri Lokupathirage <isurim2002@gmail.com>
2025-09-28 08:37:10 +0530

    day-2 added

---
+++ day-2/Day2.java
@@ -0,0 +1,9 @@
+public class Day2 {
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+		System.out.println(1234);
+		System.out.println(-1234);
+		System.out.println(20.321);
+		System.out.println(true);
+	}
+}
