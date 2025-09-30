dd219d36756f4d2a2b3ac3fd7bd99c1bd89a235f
Isuri Lokupathirage <isurim2002@gmail.com>
2025-09-30 15:36:29 +0530

    *

---
+++ day-2/Concatination.java
@@ -0,0 +1,11 @@
+public class Concatination{
+	public static void main(String[] args){
+		System.out.println("AB"+"CD"); // ABCD - String Concatination 
+		System.out.println(10+20);//30 - Arithmatic add
+		System.out.println(10+"CD");//10CD
+		System.out.println("10"+20);//1020
+		System.out.println("10"+20+10);//102010
+		System.out.println(10 + 10 + "10"+30);//201030
+		System.out.println("10"+(20+10));//1030
+	}
+}
--- day-2/Variable.java
+++ day-2/Variable.java
@@ -1,7 +1,10 @@
 public class Variable{
 	public static void main(String[] args){
-		int x; //variable declaration
-		x = 50; //variable initialization
+		int x; //variable declaration(create variable)
+		x = 50; //variable initialization(assign a value)
 		System.out.println(x);
 	}
 }
+
+//int - data type
+//x -variable name
