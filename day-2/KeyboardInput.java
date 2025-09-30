e1b5c13849a64a8242dd6c0e4c1e9e3ffc05cc52
Isuri Lokupathirage <isurim2002@gmail.com>
2025-09-30 16:46:05 +0530

    *

---
+++ day-2/KeyboardInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; // import.java.util.*;
+public class KeyboardInput {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int age = input.nextInt();
+		System.out.println("age : "+age);
+	}
+}
